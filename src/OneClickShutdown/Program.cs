6078af434661e6f5820af25aeeea32efbfab6280
SeppPenner <HansM2013@gmx.de>
2022-02-16 08:44:45 +0100

    Added nullable checks, added editorconfig, added file scoped namespaces, added global usings.

--- src/OneClickShutdown/Program.cs
+++ src/OneClickShutdown/Program.cs
@@ -1,4 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="Program.cs" company="Hämmer Electronics">
 //   Copyright (c) All rights reserved.
 // </copyright>
@@ -7,28 +7,23 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
-namespace OneClickShutdown
-{
-    using System;
-    using System.Diagnostics;
-    using System.Reflection;
+namespace OneClickShutdown;
 
+/// <summary>
+/// The main program.
+/// </summary>
+public static class Program
+{
     /// <summary>
-    /// The main program.
+    /// The main method.
     /// </summary>
-    public static class Program
+    public static void Main()
     {
-        /// <summary>
-        /// The main method.
-        /// </summary>
-        public static void Main()
-        {
-            Console.Title = Assembly.GetExecutingAssembly().GetName().Name + " " +
-                            Assembly.GetExecutingAssembly().GetName().Version;
-            Console.WriteLine("Windows wird heruntergefahren...");
-            Console.WriteLine("Beliebige Taste zum Beenden drücken...");
-            Process.Start("shutdown", "/s /t 0");
-            Console.ReadKey();
-        }
+        Console.Title = Assembly.GetExecutingAssembly().GetName().Name + " " +
+                        Assembly.GetExecutingAssembly().GetName().Version;
+        Console.WriteLine("Windows wird heruntergefahren...");
+        Console.WriteLine("Beliebige Taste zum Beenden drücken...");
+        Process.Start("shutdown", "/s /t 0");
+        Console.ReadKey();
     }
 }
